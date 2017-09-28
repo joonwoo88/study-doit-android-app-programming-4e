4a5b564c1ab575e67b1bb610117ee4dcdfe013e1
Chr0m3 <chr0m3.kr@gmail.com>
2017-09-29 07:17:33 +0900

    Add Test(2-01-1)
    Add Test(2-01-1).

---
+++ Test(2-01-1)/app/src/main/java/chr0m3/test_2_01_1/MainActivity.java
@@ -0,0 +1,21 @@
+package chr0m3.test_2_01_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String hello = new String();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
