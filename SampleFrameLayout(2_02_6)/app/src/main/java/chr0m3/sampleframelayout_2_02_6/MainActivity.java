c5bf99b27cec3aa1f19e3e02f7eb7c3a33257f1c
Chr0m3 <chr0m3.kr@gmail.com>
2017-09-30 19:04:01 +0900

    Add SampleFrameLayout(2_02_6)
    Add SampleFrameLayout(2_02_6).

---
+++ SampleFrameLayout(2_02_6)/app/src/main/java/chr0m3/sampleframelayout_2_02_6/MainActivity.java
@@ -0,0 +1,39 @@
+package chr0m3.sampleframelayout_2_02_6;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+    int imageIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+    }
+
+    public void onButton1Clicked(View v) {
+        changeImage();
+    }
+
+    private void changeImage() {
+        if (imageIndex == 0) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex = 1;
+        } else if (imageIndex == 1) {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+
+            imageIndex = 0;
+        }
+    }
+}
